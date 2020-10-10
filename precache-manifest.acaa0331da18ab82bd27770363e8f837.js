94c8ceb03fbabbeaa205f14e5001a8b8de9a9bb2
Amritanshu Dev Rawat <59793081+Developer-Amritanshu@users.noreply.github.com>
2020-10-10 22:28:16 +0530

    Updates

---
+++ precache-manifest.acaa0331da18ab82bd27770363e8f837.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a15ceed5e149ecdb75e2075d6f7c6e6",
+    "url": "/Project-Responsive-React-Page/index.html"
+  },
+  {
+    "revision": "29efd337b2ad3b318697",
+    "url": "/Project-Responsive-React-Page/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "63d4aca4c9090ed32a19",
+    "url": "/Project-Responsive-React-Page/static/css/main.aa9b56c4.chunk.css"
+  },
+  {
+    "revision": "29efd337b2ad3b318697",
+    "url": "/Project-Responsive-React-Page/static/js/2.63fe837a.chunk.js"
+  },
+  {
+    "revision": "706ae2ba09b6b4555454ecd6058d6910",
+    "url": "/Project-Responsive-React-Page/static/js/2.63fe837a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d4aca4c9090ed32a19",
+    "url": "/Project-Responsive-React-Page/static/js/main.751337e4.chunk.js"
+  },
+  {
+    "revision": "278a7a0c1cb7695fde39",
+    "url": "/Project-Responsive-React-Page/static/js/runtime-main.8be8cd8c.js"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/Project-Responsive-React-Page/static/media/img2.e981619f.svg"
+  }
+]);
